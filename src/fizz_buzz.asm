5aee92919fbe07ce84cd916f75ea3808368c1390
Ubugeeei <ubuge1122@gamil.com>
2022-05-22 15:47:20 +0900

    feat: impl fizzbuzz

--- src/fizz_buzz.asm
+++ src/fizz_buzz.asm
@@ -5,58 +5,103 @@ global	start
 section	.text
 
 start:
-	mov	rcx, 0
-	jmp	fizz_buzz_loop
+	mov	rcx, 1
+	jmp	fizz_buzz_branch
 
+fizz_buzz_branch:
+	mov	rdx, 0
+	mov	rax, rcx
+
+	mov	r15, 15
+	div	r15
+	cmp	rdx, 0
+	jne	fizz_branch
+
+	mov	rax, 0x02000004 ; syscall: write
+	mov	rdi, 0x01
+	mov	rsi, fizz_buzz ; val
+	mov	rdx, 0x0a ; len
+
+	push	rcx
+	syscall
+	pop	rcx
+
+	inc	rcx
+	cmp	rcx, 16
+	jl	fizz_buzz_branch
+
+	jmp exit
+
+
+fizz_branch:
+	mov	rdx, 0
+	mov	rax, rcx
+
+	mov	r15, 3
+	div	r15
+	cmp	rdx, 0
+	jne	buzz_branch
+
+	mov	rax, 0x02000004 ; syscall: write
+	mov	rdi, 0x01
+	mov	rsi, fizz ; val
+	mov	rdx, 0x05 ; len
+
+	push	rcx
+	syscall
+	pop	rcx
+	
+	inc	rcx
+	cmp	rcx, 16
+	jl	fizz_buzz_branch
+
+	jmp exit
+
+buzz_branch:
+	mov	rdx, 0
+	mov	rax, rcx
+
+	mov	r15, 5
+	div	r15
+	cmp	rdx, 0
+	jne	else_branch
+
+	mov	rax, 0x02000004 ; syscall: write
+	mov	rdi, 0x01
+	mov	rsi, buzz ; val
+	mov	rdx, 0x05 ; len
+
+	push	rcx
+	syscall
+	pop	rcx
+
+	inc	rcx
+	cmp	rcx, 16
+	jl	fizz_buzz_branch
+
+	jmp exit
+
+else_branch:
+	mov	rax, 0x02000004 ; syscall: write
+	mov	rdi, 0x01
+	mov	rsi, new_line ; val
+	mov	rdx, 0x01 ; len
+	push	rcx
+	syscall
+	pop	rcx
+
+
+	inc	rcx
+	cmp	rcx, 16
+	jl	fizz_buzz_branch
+
+	jmp exit
 
 exit:
 	mov	rax, 0x02000001 ; syscall: exit
 	xor	rdi, rdi
 	syscall
 	
-
-fizz_buzz_loop:
-	mov	rax, 0x02000004 ; syscall: write
-	mov	rdi, 0x01
-	mov	rsi, fizz; val
-	mov	rdx, 0x05 ; len
-	push rcx
-	syscall
-
-
-	; TODO fizz_buzz proccess
-
-
-	pop	rcx
-	inc	rcx
-	cmp	rcx, 10
-	jl	fizz_buzz_loop
-
-	jmp	exit
-
-print_fizz:
-	mov	rax, 0x02000004 ; syscall: write
-	mov	rdi, 0x01
-	mov	rsi, fizz ; val
-	mov	rdx, 0x05 ; len
-	syscall
-	jmp	exit
-
-print_buzz:
-	mov	rax, 0x02000004 ; syscall: write
-	mov	rdi, 0x01
-	mov	rsi, buzz ; val
-	mov	rdx, 0x05 ; len
-	syscall
-	jmp	exit
-
-print_fizz_buzz:
-	mov	rax, 0x02000004 ; syscall: write
-	mov	rdi, 0x01
-	mov	rsi, fizz_buzz ; val
-	mov	rdx, 0x0a ; len
-	syscall
-	jmp	exit
 
 section	.data
 
@@ -68,3 +113,6 @@ buzz:
 
 fizz_buzz:
 	db	"fizz_buzz", 0x0a
+
+new_line:
+	db	0x0a
